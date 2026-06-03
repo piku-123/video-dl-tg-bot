2eb110412511a9118edb0f1b7334b7dcad4f16aa
piku-123 <zeroex143@gmail.com>
2026-06-03 10:20:32 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-const TOKEN = process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE';
-const BOT_USERNAME = process.env.BOT_USERNAME || 'Zeroex Video Downloader';
-const DL_API = process.env.DL_API || 'https://zeroex-all-rest-api.onrender.com/api/dl/download?url=';
-
 module.exports = {
-    TOKEN,
-    BOT_USERNAME,
-    DL_API
+    TOKEN: process.env.TOKEN || 'YOUR_TOKEN',
+    BOT_USERNAME: process.env.BOT_USERNAME || 'Zeroex Video Downloader',
+    DL_API: process.env.DL_API || 'https://zeroex-all-rest-api.onrender.com/api/dl/download?url=',
+    BASE_URL: process.env.BASE_URL || 'https://video-dl-tg-bot.onrender.com'  // ← এইটা যোগ করো
 };
